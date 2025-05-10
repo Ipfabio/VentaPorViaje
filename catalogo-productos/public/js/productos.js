1df41480baf621abcca05384f39d29b3b36e6b87
Aron <Aron@mail.com>
2025-05-10 15:33:35 -0300

    modifico el nombre de las imagenes y pongo ejemplos de items

--- catalogo-productos/public/js/productos.js
+++ catalogo-productos/public/js/productos.js
@@ -8,6 +8,52 @@ const productos = [
     precio: 85000,
     descripcion: "Capacidad 6L, 1700W. Casi nueva. Funciona perfecto.",
     imagenes: [
+      "images/cocina/FreidoraDeAire/arriba.png",
+      "images/cocina/FreidoraDeAire/frente.png",
+      "images/cocina/FreidoraDeAire/ejemplo.png"
+    ]
+  },
+  {
+    id: 2,
+    categoria: "cocina",
+    nombre: "Licuadora",
+    precio: 45000,
+    descripcion: "Tapizado gris, muy cómodo. Poco uso.",
+    imagenes: [
+      "images/cocina/Licuadora/arriba.png",
+      "images/cocina/Licuadora/Lejos.png"
+    ]
+  },
+    {
+    id: 3,
+    categoria: "cocina",
+    nombre: "Microondas",
+    precio: 85000,
+    descripcion: "Capacidad 6L, 1700W. Casi nueva. Funciona perfecto.",
+    imagenes: [
+      "images/cocina/microondas/abierto.png",
+      "images/cocina/microondas/botonera.png",
+      "images/cocina/microondas/frente.png"
+    ]
+  },
+  {
+    id: 2,
+    categoria: "living",
+    nombre: "Sillón 2 cuerpos",
+    precio: 45000,
+    descripcion: "Tapizado gris, muy cómodo. Poco uso.",
+    imagenes: [
+      "images/living/Sillon1.png",
+      "images/living/Sillon2.png"
+    ]
+  },
+    {
+    id: 1,
+    categoria: "cocina",
+    nombre: "Freidora Sin Aceite Cecotec",
+    precio: 85000,
+    descripcion: "Capacidad 6L, 1700W. Casi nueva. Funciona perfecto.",
+    imagenes: [
       "images/cocina/FreidoraDeAire/FreidoraDeAire.png",
       "images/cocina/FreidoraDeAire/FreidoraDeAire-Arriba.png",
       "images/cocina/FreidoraDeAire/FreidoraDeAire-Ejemplo.png"
@@ -23,5 +69,5 @@ const productos = [
       "images/living/Sillon1.png",
       "images/living/Sillon2.png"
     ]
-  }
+  },
 ];
