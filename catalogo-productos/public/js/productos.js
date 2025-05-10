d662ff738a24a2e2dc07e6dece0e588d9b7e033a
Aron <Aron@mail.com>
2025-05-10 14:19:08 -0300

    Agrego algunos productos de prueba

--- catalogo-productos/public/js/productos.js
+++ catalogo-productos/public/js/productos.js
@@ -13,8 +13,69 @@ const productos = [
     ]
   },
   {
+    id: 1,
+    categoria: "cocina",
+    nombre: "Freidora Sin Aceite Cecotec",
+    precio: 85000,
+    descripcion: "Capacidad 6L, 1700W. Casi nueva. Funciona perfecto.",
+    imagenes: [
+      "../public/images/cocina/Freidora1.png",
+      "../public/images/cocina/Freidora2.png"
+    ]
+  },
+  {
     id: 2,
     categoria: "living",
+    nombre: "Sillón 2 cuerpos",
+    precio: 45000,
+    descripcion: "Tapizado gris, muy cómodo. Poco uso.",
+    imagenes: [
+      "../public/images/living/Sillon1.png"
+    ]
+  },
+    {
+    id: 2,
+    categoria: "living",
+    nombre: "Sillón 2 cuerpos",
+    precio: 45000,
+    descripcion: "Tapizado gris, muy cómodo. Poco uso.",
+    imagenes: [
+      "../public/images/living/Sillon1.png"
+    ]
+  },
+  {
+    id: 3,
+    categoria: "oficina",
+    nombre: "Sillón 2 cuerpos",
+    precio: 45000,
+    descripcion: "Tapizado gris, muy cómodo. Poco uso.",
+    imagenes: [
+      "../public/images/living/Sillon1.png"
+    ]
+  },
+    {
+    id: 3,
+    categoria: "oficina",
+    nombre: "Sillón 2 cuerpos",
+    precio: 45000,
+    descripcion: "Tapizado gris, muy cómodo. Poco uso.",
+    imagenes: [
+      "../public/images/living/Sillon1.png"
+    ]
+  },
+  {
+    id: 4,
+    categoria: "pieza",
+    nombre: "Sillón 2 cuerpos",
+    precio: 45000,
+    descripcion: "Tapizado gris, muy cómodo. Poco uso.",
+    imagenes: [
+      "../public/images/living/Sillon1.png"
+    ]
+  },
+  {
+    id: 4,
+    categoria: "pieza",
     nombre: "Sillón 2 cuerpos",
     precio: 45000,
     descripcion: "Tapizado gris, muy cómodo. Poco uso.",
